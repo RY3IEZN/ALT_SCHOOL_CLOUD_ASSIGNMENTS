ad52ae194f370f25c06ad47962d9363cfec8c5af
unknown <ghostlordwalker@gmail.com>
2023-09-04 16:17:27 +0100

    tested changes

--- Vagrant_Ubuntu_Cluster_Project_30Aug23/copy_mnt_slave.sh
+++ Vagrant_Ubuntu_Cluster_Project_30Aug23/copy_mnt_slave.sh
@@ -7,7 +7,5 @@ sudo mkdir -p /mnt/altschool
 echo "see you on the other side" | sudo tee /mnt/altschool/content
 
 
-ssh ghost@192.168.56.4 "mkdir -p /mnt/altschool/slave"
-
 # Copy the content from master to slave
-sudo -u altschool scp -r /mnt/altschool/ altschool@slave:/mnt/altschool/slave/
+sudo -u altschool scp -r /mnt/altschool/ altschool@192.168.56.4:/mnt/altschool/slave/
