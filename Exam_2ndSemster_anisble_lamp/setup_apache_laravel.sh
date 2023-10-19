f043eeb1c20eafba293b47042b00d891145a8594
unknown <unekue2@gmail.com>
2023-10-19 13:17:28 +0100

    added host file, modified scripts

--- Exam_2ndSemster_anisble_lamp/setup_apache_laravel.sh
+++ Exam_2ndSemster_anisble_lamp/setup_apache_laravel.sh
@@ -3,20 +3,22 @@
 # clone the repo
 echo "cloning the repo"
 git clone https://github.com/laravel/laravel
-sleep 5
+sleep 3
 
 # enter into the repo
-echo 
+echo "entering the cloned repo"
 cd laravel
 
 # check directory
 echo $(pwd)
-sleep 5
+sleep 3
 
 
 # rename .env 
+echo "rename .env"
 mv .env.example .env
 
+# update app_key in .env
 new_key=base64:ZXYwcmV1bGVhcjZ3N2NzdjJybzFiZzJ0cTU0ZnU3a2k=
 # Check if the new key is not empty
 if [ -n "$new_key" ]; then
@@ -28,17 +30,21 @@ if [ -n "$new_key" ]; then
 else
     echo "Failed to retrieve a new key from the website."
 fi
+sleep 3
 
 # install composer dependencies
+echo "installing php and composer dependencies"
 sudo apt-get install php-xml php- -y
 sudo apt install php-cli php-json php-common curl php-mbstring php-zip unzip php-curl php-xml -y
-sleep 5
+sleep 3
 
 # install composer
+echo "installing composer"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # update the repo
+echo "install repo depencies"
 composer install
 
 # update the repo
@@ -51,33 +57,37 @@ composer update
 # php artisan serve
 
 # disable the default apache page
+echo "disable default apache page"
 sudo a2dissite 000-default.conf
 
 # navigate to sites-availble
+echo "navigate to sites-availble"
 cd /etc/apache2/sites-available
 
 # check directory
 echo $(pwd)
 
 # create your site file
-sudo touch laravel.conf
+echo "Create sitename.conf"
+sudo touch <site-name>.conf
 
 # update the content
-sudo sed -n 'w laravel.conf' <<EOF
+echo "update file"
+sudo sed -n 'w <site-name>.conf' <<EOF
 <VirtualHost *:80>
-    ServerAdmin uneku@ejig.com
-    ServerName laravel.local
+    ServerAdmin admin@admin.com
+    ServerName <site-name>.local
 
-    DocumentRoot /var/www/laravel/public
+    DocumentRoot /var/www/<site-name>/public
 
-    <Directory /var/www/laravel/public>
+    <Directory /var/www/<site-name>/public>
         Options -Indexes +FollowSymLinks
         AllowOverride All
         Require all granted
     </Directory>
 
-    ErrorLog \${APACHE_LOG_DIR}/laravel-error.log
-    CustomLog \${APACHE_LOG_DIR}/laravel-access.log combined
+    ErrorLog \${APACHE_LOG_DIR}/<site-name>-error.log
+    CustomLog \${APACHE_LOG_DIR}/<site-name>-access.log combined
 
     <IfModule mod_dir.c>
         DirectoryIndex index.php
@@ -87,26 +97,29 @@ EOF
 
 
 # enable the site
-sudo a2ensite laravel
+echo "enabling sitename"
+sudo a2ensite <site-name>
 
 # create the site directory
-sudo mkdir -p /var/www/laravel
+echo "create site directory"
+sudo mkdir -p /var/www/<site-name>
 
 # copy the content to site directory
 cd
 echo $(pwd)
-sudo cp -R laravel/* /var/www/laravel/
+sudo cp -R laravel/* /var/www/<site-name>/
 
 # go back to the directory
-cd /var/www/laravel
+cd /var/www/<site-name>
 echo $(pwd)
 
 # set permission for the files
-sudo chown -R vagrant:www-data /var/www/laravel/
-sudo find /var/www/laravel/ -type f -exec chmod 664 {} \;
-sudo find /var/www/laravel/ -type d -exec chmod 775 {} \;
+echo "grant permission to files"
+sudo chown -R <server-user>:www-data /var/www/<site-name>/
+sudo find /var/www/<site-name>/ -type f -exec chmod 664 {} \;
+sudo find /var/www/<site-name>/ -type d -exec chmod 775 {} \;
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
 
 # done
-echo 'webserver is up vist vm ip address'
+echo 'webserver is up vist http://127.0.0.1 to view website'
