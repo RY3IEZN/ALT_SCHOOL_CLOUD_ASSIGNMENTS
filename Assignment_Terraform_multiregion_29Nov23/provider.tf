4bd495a5696c7eb223f076b44726a044aa489620
unknown <unekue2@gmail.com>
2023-11-30 10:28:35 +0100

    updated template file

--- Assignment_Terraform_multiregion_29Nov23/provider.tf
+++ Assignment_Terraform_multiregion_29Nov23/provider.tf
@@ -11,13 +11,13 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAXQSK72JAFBAY6LEH"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 provider "aws" {
   alias      = "london"
-  region     = "eu-west-2"
-  access_key = "AKIAXQSK72JAFBAY6LEH"
-  secret_key = "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"
+  region     = "eu-central-1"
+  access_key = ""
+  secret_key = ""
 }
