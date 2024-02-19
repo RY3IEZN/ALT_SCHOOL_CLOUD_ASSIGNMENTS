3e51dfa4e6f8e91025392025d906067715ce5590
unknown <unekue2@gmail.com>
2024-02-19 15:42:45 +0100

    folder struct

---
+++ Final_Capstone_project/install/README.md
@@ -0,0 +1,5 @@
+<!-- @format -->
+
+# Install utilities
+
+Contains utility scripts for preparing a target platform.
---
+++ Final_Capstone_project/install/tools.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# update OS repository
+sudo apt update -y
+
+# install docker
+install docker
+sudo apt-get update -y &&
+sudo apt-get install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common &&
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+sudo apt-get update -y &&
+sudo sudo apt-get install docker-ce docker-ce-cli containerd.io -y &&
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+# install kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install azure cli
+# https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# install argocd
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
